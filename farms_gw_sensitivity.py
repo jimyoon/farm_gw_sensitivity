2f2fad395322a76a3e787b66886b72146289929d
jimyoon <jimyoon@gmail.com>
2022-10-04 11:11:36 -0700

    Major update for incorporation into sensitivity workflow (working prototype)

--- farms_gw_sensitivity.py
+++ farms_gw_sensitivity.py
@@ -6,6 +6,12 @@ try:
     import cPickle as pickle
 except ImportError:  # python 3.x
     import pickle
+import numpy as np
+from SALib.sample import saltelli
+from SALib.analyze import sobol
+import time
+
+time_start = time.time()
 
 ##### Adjust pandas setting for debugging
 pd.set_option('display.max_columns', None)
@@ -48,7 +54,7 @@ gw_cost_curves = pd.read_csv('./data_inputs/20220830_gw_cost_curves.csv')
 gw_cost_lookup = pd.read_csv('./data_inputs/20220830_gw_cost_lookup.csv')
 
 ##### Select farm/gw run options (these are currently implemented as higher-level options than the sensitivity inputs)
-farm_id = [15557]  # Pick the farm agent to use as a basis for the sensitivity analysis
+farm_id = [15557]  # Pick the farm agent [0-53834] to use as a basis for the sensitivity analysis
 gw_cost_id = 'gw7'  # Pick the gw cost curve to use as a basis for the sensitivity analysis
 gw_area_well_sqm = 750 * 750  # Assumed groundwater area irrigated with a well in square meters
 gw_area_well_acres = gw_area_well_sqm * 0.000247105  # Assumed groundwater area irrigated with a well in acres
@@ -85,153 +91,199 @@ for key,value in gammas_total_subset.items():
     if value == 0:
         net_prices_land_subset[key] = -9999999999
 
+
+time_load = time.time()
+
 ###### AFTER TESTING CONVERT FOLLOWING INTO A FUNCTION FOR INCORPORATION INTO SALIB
 
-# initialize counters/trackers
-first = True
-cumul_gw_sum = 0
-gw_multiplier = 1
+def farm_gw_model(nir_mult, sw_mult):  # currently only picking a couple variables to test (applied uniformly across all crops)
 
-# start simulation time loop
-for t in range(no_of_years):
-    print(t)
+    # apply sensitivity multipliers
+    # for key in gammas_total_subset:
+    #     gammas_total_subset[key] *= gammas_mult
 
-    # initialize pyomo model
-    fwm_s = ConcreteModel()
-    fwm_s.ids = Set(initialize=ids_subset_sorted)
-    if first:
-        fwm_s.net_prices_gw = Param(fwm_s.ids, initialize=net_prices_gw_subset, mutable=True)
-    else:
-        net_prices_gw_subset_update = net_prices_gw_subset.copy()
-        net_prices_gw_subset_update.update((x, y * gw_multiplier) for x, y in net_prices_gw_subset_update.items())
-        fwm_s.net_prices_gw = Param(fwm_s.ids, initialize=net_prices_gw_subset_update, mutable=True)
-    fwm_s.farm_ids = Set(initialize=farm_id)
-    fwm_s.crop_ids_by_farm = Set(fwm_s.farm_ids, initialize=crop_ids_by_farm_subset)
-    fwm_s.crop_ids_by_farm_and_constraint = Set(fwm_s.farm_ids, initialize=crop_ids_by_farm_subset)
-    fwm_s.net_prices_sw = Param(fwm_s.ids, initialize=net_prices_sw_subset, mutable=True)
-    fwm_s.net_prices_total = Param(fwm_s.ids, initialize=net_prices_land_subset, mutable=True)
-    fwm_s.gammas_total = Param(fwm_s.ids, initialize=gammas_total_subset, mutable=True)
-    fwm_s.land_constraints = Param(fwm_s.farm_ids, initialize=max_land_constr_subset, mutable=True)
-    fwm_s.sw_constraints = Param(fwm_s.farm_ids, initialize=sw_calib_constr_subset, mutable=True)
-    fwm_s.gw_constraints = Param(fwm_s.farm_ids, initialize=gw_calib_constr_subset, mutable=True)
-    x_start_values = dict(enumerate([0.0] * 3))
-    fwm_s.xs_total = Var(fwm_s.ids, domain=NonNegativeReals, initialize=x_start_values)
-    fwm_s.xs_sw = Var(fwm_s.ids, domain=NonNegativeReals, initialize=x_start_values)
-    fwm_s.xs_gw = Var(fwm_s.ids, domain=NonNegativeReals, initialize=x_start_values)
-    fwm_s.nirs = Param(fwm_s.ids, initialize=nirs_subset, mutable=True)
+    for key in nirs_subset:
+        nirs_subset[key] *= nir_mult
 
-    # define pyomo objective function
-    def obj_fun(fwm_s):
-        return 0.00001 * sum(sum((fwm_s.net_prices_total[h] * fwm_s.xs_total[h] - 0.5 * fwm_s.gammas_total[h] *
-                                  fwm_s.xs_total[h] * fwm_s.xs_total[h]) for h in fwm_s.crop_ids_by_farm[f]) +
-                             sum((fwm_s.net_prices_sw[i] * fwm_s.xs_sw[i]) for i in fwm_s.crop_ids_by_farm[f]) +
-                             sum((fwm_s.net_prices_gw[g] * fwm_s.xs_gw[g]) for g in fwm_s.crop_ids_by_farm[f]) for f
-                             in fwm_s.farm_ids)
+    for key in sw_calib_constr_subset:
+        sw_calib_constr_subset[key] *= sw_mult
 
-    fwm_s.obj_f = Objective(rule=obj_fun, sense=maximize)
+    # initialize counters/trackers
+    first = True
+    cumul_gw_sum = 0
+    gw_multiplier = 1
 
-    # define constraints
-    def land_constraint(fwm_s, ff):
-        return sum(fwm_s.xs_total[i] for i in fwm_s.crop_ids_by_farm_and_constraint[ff]) <= \
-               fwm_s.land_constraints[ff]
+    # start simulation time loop
+    for t in range(no_of_years):
+        print('year' + str(t))
+        print('!!JY!! ' + str(nir_mult))
 
-    fwm_s.c1 = Constraint(fwm_s.farm_ids, rule=land_constraint)
-
-    def obs_lu_constraint_sum(fwm_s, i):
-        return fwm_s.xs_sw[i] + fwm_s.xs_gw[i] == fwm_s.xs_total[i]
-
-    fwm_s.c5 = Constraint(fwm_s.ids, rule=obs_lu_constraint_sum)
-
-    def sw_constraint(fwm_s, ff):
-        return sum(fwm_s.xs_sw[i] * fwm_s.nirs[i] for i in fwm_s.crop_ids_by_farm_and_constraint[ff]) <= \
-               fwm_s.sw_constraints[ff]
-
-    fwm_s.c2 = Constraint(fwm_s.farm_ids, rule=sw_constraint)
-
-    def gw_constraint(fwm_s, ff):
-        return sum(fwm_s.xs_gw[i] * fwm_s.nirs[i] for i in fwm_s.crop_ids_by_farm_and_constraint[ff]) <= \
-               fwm_s.gw_constraints[ff]
-
-    fwm_s.c6 = Constraint(fwm_s.farm_ids, rule=gw_constraint)
-
-    # create and run the optimization solver
-    opt = SolverFactory("ipopt", solver_io='nl')
-    results = opt.solve(fwm_s, keepfiles=False, tee=True)
-    print(results.solver.termination_condition)
-
-    # store main model outputs
-    result_xs_sw = dict(fwm_s.xs_sw.get_values())
-    result_xs_gw = dict(fwm_s.xs_gw.get_values())
-    result_xs_total = dict(fwm_s.xs_total.get_values())
-
-    farms_list = [farms_master.loc[[farm_id[0]]].nldas.values[0]]
-
-    # process groundwater production results and
-    results_pd = farms_master[farms_master['nldas'].isin(farms_list)]
-    results_pd['xs_gw'] = 0
-    results_pd['xs_sw'] = 0
-    results_pd['xs_total'] = 0
-    results_pd['id'] = results_pd['index']
-    results_xs_sw_pd = pd.DataFrame.from_dict(result_xs_sw, orient='index')
-    results_xs_sw_pd['id'] = results_xs_sw_pd.index + 1
-    results_xs_sw_pd = results_xs_sw_pd.rename(columns={0: "xs_sw_temp"})
-    results_pd = results_pd.merge(results_xs_sw_pd[['id', 'xs_sw_temp']], how='left', on=['id'])
-    results_pd.loc[results_pd['xs_sw_temp'].notnull(), 'xs_sw'] = results_pd['xs_sw_temp']
-    results_xs_gw_pd = pd.DataFrame.from_dict(result_xs_gw, orient='index')
-    results_xs_gw_pd['id'] = results_xs_gw_pd.index + 1
-    results_xs_gw_pd = results_xs_gw_pd.rename(columns={0: "xs_gw_temp"})
-    results_pd = results_pd.merge(results_xs_gw_pd[['id', 'xs_gw_temp']], how='left', on=['id'])
-    results_pd.loc[results_pd['xs_gw_temp'].notnull(), 'xs_gw'] = results_pd['xs_gw_temp']
-    results_xs_total_pd = pd.DataFrame.from_dict(result_xs_total, orient='index')
-    results_xs_total_pd['id'] = results_xs_total_pd.index + 1
-    results_xs_total_pd = results_xs_total_pd.rename(columns={0: "xs_total_temp"})
-    results_pd = results_pd.merge(results_xs_total_pd[['id', 'xs_total_temp']], how='left', on=['id'])
-    results_pd.loc[results_pd['xs_total_temp'].notnull(), 'xs_total'] = results_pd['xs_total_temp']
-    results_pd = results_pd.drop(['xs_gw_temp', 'xs_sw_temp', 'xs_total_temp'], axis=1)
-    results_pd['year'] = t+1
-    results_pd['gw_vol'] = results_pd['xs_gw'] * results_pd['nir_corrected']
-    try:
-        results_pd['gw_mult'] = gw_multiplier
-    except NameError:
-        results_pd['gw_mult'] = 1
-    try:
-        results_pd['gw_cumul_vol'] = cumul_gw_sum
-    except NameError:
-        results_pd['gw_cumul_vol'] = 0
-    results_pd['gw_run'] = gw_cost_id
-    results_pd['trans'] = gw_cost_lookup[(gw_cost_lookup.row_indx == gw_cost_id)]['Trans'].values[0]
-    if first:
-        results_combined = results_pd
-    else:
-        results_combined = pd.concat([results_combined, results_pd])
-    aggregation_functions = {'gw_vol': 'sum', 'xs_gw': 'sum'}
-    gw_sum = results_pd[['nldas','gw_vol','xs_gw']].groupby(['nldas']).aggregate(aggregation_functions)
-    gw_sum = gw_sum.reset_index()
-    gw_sum = gw_sum.merge(farm_gw_sum[['nldas', 'no_of_wells']], how='left', on=['nldas'])
-    gw_sum['gw_vol_well'] = gw_sum['gw_vol'] * farms_per_grid / gw_sum['no_of_wells']
-    gw_sum['gw_vol_well_km3'] = gw_sum['gw_vol_well'] * 1.23348e-6
-    if first:
-        cumul_gw_sum = gw_sum['gw_vol_well_km3'].values[0]
-    else:
-        cumul_gw_sum += gw_sum['gw_vol_well_km3'].values[0]
-    i = 0
-    for index, row in gw_cost_curves.iterrows():
-        if cumul_gw_sum > row['volume']:
-            i = index
+        # initialize pyomo model
+        fwm_s = ConcreteModel()
+        fwm_s.ids = Set(initialize=ids_subset_sorted)
+        if first:
+            fwm_s.net_prices_gw = Param(fwm_s.ids, initialize=net_prices_gw_subset, mutable=True)
         else:
-            break
+            net_prices_gw_subset_update = net_prices_gw_subset.copy()
+            net_prices_gw_subset_update.update((x, y * gw_multiplier) for x, y in net_prices_gw_subset_update.items())
+            fwm_s.net_prices_gw = Param(fwm_s.ids, initialize=net_prices_gw_subset_update, mutable=True)
+        fwm_s.farm_ids = Set(initialize=farm_id)
+        fwm_s.crop_ids_by_farm = Set(fwm_s.farm_ids, initialize=crop_ids_by_farm_subset)
+        fwm_s.crop_ids_by_farm_and_constraint = Set(fwm_s.farm_ids, initialize=crop_ids_by_farm_subset)
+        fwm_s.net_prices_sw = Param(fwm_s.ids, initialize=net_prices_sw_subset, mutable=True)
+        fwm_s.net_prices_total = Param(fwm_s.ids, initialize=net_prices_land_subset, mutable=True)
+        fwm_s.gammas_total = Param(fwm_s.ids, initialize=gammas_total_subset, mutable=True)
+        fwm_s.land_constraints = Param(fwm_s.farm_ids, initialize=max_land_constr_subset, mutable=True)
+        fwm_s.sw_constraints = Param(fwm_s.farm_ids, initialize=sw_calib_constr_subset, mutable=True)
+        fwm_s.gw_constraints = Param(fwm_s.farm_ids, initialize=gw_calib_constr_subset, mutable=True)
+        x_start_values = dict(enumerate([0.0] * 3))
+        fwm_s.xs_total = Var(fwm_s.ids, domain=NonNegativeReals, initialize=x_start_values)
+        fwm_s.xs_sw = Var(fwm_s.ids, domain=NonNegativeReals, initialize=x_start_values)
+        fwm_s.xs_gw = Var(fwm_s.ids, domain=NonNegativeReals, initialize=x_start_values)
+        fwm_s.nirs = Param(fwm_s.ids, initialize=nirs_subset, mutable=True)
 
-    if gw_cost_curves.loc[i][gw_cost_id] != 0:  # JY temp to deal with zeros in groundwater cost curves (check in with Stephen)
-        gw_multiplier = gw_cost_curves.loc[i][gw_cost_id]
-    else:
-        gw_multiplier = 9999999999999  # Set groundwater cost to extremely high value to reflect groundwater exhaustion
-    print(cumul_gw_sum)
-    first = False
+        # define pyomo objective function
+        def obj_fun(fwm_s):
+            return 0.00001 * sum(sum((fwm_s.net_prices_total[h] * fwm_s.xs_total[h] - 0.5 * fwm_s.gammas_total[h] *
+                                      fwm_s.xs_total[h] * fwm_s.xs_total[h]) for h in fwm_s.crop_ids_by_farm[f]) +
+                                 sum((fwm_s.net_prices_sw[i] * fwm_s.xs_sw[i]) for i in fwm_s.crop_ids_by_farm[f]) +
+                                 sum((fwm_s.net_prices_gw[g] * fwm_s.xs_gw[g]) for g in fwm_s.crop_ids_by_farm[f]) for f
+                                 in fwm_s.farm_ids)
 
-#  Calculate summary results (percent change in total irrigated area as an initial result)
-aggregation_functions = {'xs_total': 'sum'}
-summary_pd = results_combined[['nldas','year','xs_total']].groupby(['nldas','year']).aggregate(aggregation_functions)
-summary_pd = summary_pd.reset_index()
-year_start = summary_pd.year.min()
-year_end = summary_pd.year.max()
-end_div_start_area = summary_pd[(summary_pd.year==year_end)].xs_total.values[0] / summary_pd[(summary_pd.year == year_start)].xs_total.values[0]
+        fwm_s.obj_f = Objective(rule=obj_fun, sense=maximize)
+
+        # define constraints
+        def land_constraint(fwm_s, ff):
+            return sum(fwm_s.xs_total[i] for i in fwm_s.crop_ids_by_farm_and_constraint[ff]) <= \
+                   fwm_s.land_constraints[ff]
+
+        fwm_s.c1 = Constraint(fwm_s.farm_ids, rule=land_constraint)
+
+        def obs_lu_constraint_sum(fwm_s, i):
+            return fwm_s.xs_sw[i] + fwm_s.xs_gw[i] == fwm_s.xs_total[i]
+
+        fwm_s.c5 = Constraint(fwm_s.ids, rule=obs_lu_constraint_sum)
+
+        def sw_constraint(fwm_s, ff):
+            return sum(fwm_s.xs_sw[i] * fwm_s.nirs[i] for i in fwm_s.crop_ids_by_farm_and_constraint[ff]) <= \
+                   fwm_s.sw_constraints[ff]
+
+        fwm_s.c2 = Constraint(fwm_s.farm_ids, rule=sw_constraint)
+
+        def gw_constraint(fwm_s, ff):
+            return sum(fwm_s.xs_gw[i] * fwm_s.nirs[i] for i in fwm_s.crop_ids_by_farm_and_constraint[ff]) <= \
+                   fwm_s.gw_constraints[ff]
+
+        fwm_s.c6 = Constraint(fwm_s.farm_ids, rule=gw_constraint)
+
+        fwm_s.scaling_factor = Suffix(direction=Suffix.EXPORT)
+        fwm_s.scaling_factor[fwm_s.xs_total] = 0.0001
+        fwm_s.scaling_factor[fwm_s.xs_sw] = 0.0001
+        fwm_s.scaling_factor[fwm_s.land_constraints] = 0.000001
+        fwm_s.scaling_factor[fwm_s.sw_constraints] = 0.01
+        fwm_s.scaling_factor[fwm_s.gw_constraints] = 0.01
+        fwm_s.scaling_factor[fwm_s.c1] = 0.000001
+        fwm_s.scaling_factor[fwm_s.c5] = 0.000001
+        fwm_s.scaling_factor[fwm_s.c2] = 0.01
+        fwm_s.scaling_factor[fwm_s.c6] = 0.01
+
+        # create and run the optimization solver
+        # opt = SolverFactory("ipopt", solver_io='nl')
+        opt = SolverFactory("gurobi", solver_io='python')
+        results = opt.solve(fwm_s, keepfiles=False, tee=True)
+        print(results.solver.termination_condition)
+
+        # store main model outputs
+        result_xs_sw = dict(fwm_s.xs_sw.get_values())
+        result_xs_gw = dict(fwm_s.xs_gw.get_values())
+        result_xs_total = dict(fwm_s.xs_total.get_values())
+
+        farms_list = [farms_master.loc[[farm_id[0]]].nldas.values[0]]
+
+        # process groundwater production results and
+        results_pd = farms_master[farms_master['nldas'].isin(farms_list)]
+        results_pd['xs_gw'] = 0
+        results_pd['xs_sw'] = 0
+        results_pd['xs_total'] = 0
+        results_pd['id'] = results_pd['index']
+        results_xs_sw_pd = pd.DataFrame.from_dict(result_xs_sw, orient='index')
+        results_xs_sw_pd['id'] = results_xs_sw_pd.index + 1
+        results_xs_sw_pd = results_xs_sw_pd.rename(columns={0: "xs_sw_temp"})
+        results_pd = results_pd.merge(results_xs_sw_pd[['id', 'xs_sw_temp']], how='left', on=['id'])
+        results_pd.loc[results_pd['xs_sw_temp'].notnull(), 'xs_sw'] = results_pd['xs_sw_temp']
+        results_xs_gw_pd = pd.DataFrame.from_dict(result_xs_gw, orient='index')
+        results_xs_gw_pd['id'] = results_xs_gw_pd.index + 1
+        results_xs_gw_pd = results_xs_gw_pd.rename(columns={0: "xs_gw_temp"})
+        results_pd = results_pd.merge(results_xs_gw_pd[['id', 'xs_gw_temp']], how='left', on=['id'])
+        results_pd.loc[results_pd['xs_gw_temp'].notnull(), 'xs_gw'] = results_pd['xs_gw_temp']
+        results_xs_total_pd = pd.DataFrame.from_dict(result_xs_total, orient='index')
+        results_xs_total_pd['id'] = results_xs_total_pd.index + 1
+        results_xs_total_pd = results_xs_total_pd.rename(columns={0: "xs_total_temp"})
+        results_pd = results_pd.merge(results_xs_total_pd[['id', 'xs_total_temp']], how='left', on=['id'])
+        results_pd.loc[results_pd['xs_total_temp'].notnull(), 'xs_total'] = results_pd['xs_total_temp']
+        results_pd = results_pd.drop(['xs_gw_temp', 'xs_sw_temp', 'xs_total_temp'], axis=1)
+        results_pd['year'] = t+1
+        results_pd['gw_vol'] = results_pd['xs_gw'] * results_pd['nir_corrected']
+        try:
+            results_pd['gw_mult'] = gw_multiplier
+        except NameError:
+            results_pd['gw_mult'] = 1
+        try:
+            results_pd['gw_cumul_vol'] = cumul_gw_sum
+        except NameError:
+            results_pd['gw_cumul_vol'] = 0
+        results_pd['gw_run'] = gw_cost_id
+        results_pd['trans'] = gw_cost_lookup[(gw_cost_lookup.row_indx == gw_cost_id)]['Trans'].values[0]
+        if first:
+            results_combined = results_pd
+        else:
+            results_combined = pd.concat([results_combined, results_pd])
+        aggregation_functions = {'gw_vol': 'sum', 'xs_gw': 'sum'}
+        gw_sum = results_pd[['nldas','gw_vol','xs_gw']].groupby(['nldas']).aggregate(aggregation_functions)
+        gw_sum = gw_sum.reset_index()
+        gw_sum = gw_sum.merge(farm_gw_sum[['nldas', 'no_of_wells']], how='left', on=['nldas'])
+        gw_sum['gw_vol_well'] = gw_sum['gw_vol'] * farms_per_grid / gw_sum['no_of_wells']
+        gw_sum['gw_vol_well_km3'] = gw_sum['gw_vol_well'] * 1.23348e-6
+        if first:
+            cumul_gw_sum = gw_sum['gw_vol_well_km3'].values[0]
+        else:
+            cumul_gw_sum += gw_sum['gw_vol_well_km3'].values[0]
+        i = 0
+        for index, row in gw_cost_curves.iterrows():
+            if cumul_gw_sum > row['volume']:
+                i = index
+            else:
+                break
+
+        if gw_cost_curves.loc[i][gw_cost_id] != 0:  # JY temp to deal with zeros in groundwater cost curves (check in with Stephen)
+            gw_multiplier = gw_cost_curves.loc[i][gw_cost_id]
+        else:
+            gw_multiplier = 9999999999999  # Set groundwater cost to extremely high value to reflect groundwater exhaustion
+        print(cumul_gw_sum)
+        first = False
+
+    #  Calculate summary results (percent change in total irrigated area as an initial result)
+    aggregation_functions = {'xs_total': 'sum'}
+    summary_pd = results_combined[['nldas','year','xs_total']].groupby(['nldas','year']).aggregate(aggregation_functions)
+    summary_pd = summary_pd.reset_index()
+    year_start = summary_pd.year.min()
+    year_end = summary_pd.year.max()
+    end_div_start_area = summary_pd[(summary_pd.year==year_end)].xs_total.values[0] / summary_pd[(summary_pd.year == year_start)].xs_total.values[0]
+
+    return end_div_start_area
+
+##### Run sensitivity analysis using SALib
+
+problem = {
+    'num_vars': 2,
+    'names': ['nir_mult','sw_mult'],
+    'bounds': [[0.8, 1.2], [0.8, 1.2]]
+}
+
+param_values = saltelli.sample(problem, 2**6)
+
+y = np.array([farm_gw_model(*params) for params in param_values])
+
+time_ensemble = time.time()
+
+sobol_indices = sobol.analyze(problem, y)
